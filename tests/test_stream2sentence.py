5820cb6baed75c9b2667f8e59ff4139fac162719
Kolja Beigel <kolja.beigel@web.de>
2023-11-29 01:01:10 +0100

    v0.2.0 add another sentence tokenizer to support more languages (e.g. chinese)

--- tests/test_stream2sentence.py
+++ tests/test_stream2sentence.py
@@ -12,7 +12,7 @@ class TestSentenceGenerator(unittest.TestCase):
     def test_generator(self):
         def generator():
             yield "Hallo, "
-            yield "wie geht es dir?"
+            yield "wie geht es dir? "
             yield "Mir geht es gut."
         expected = ["Hallo,", "wie geht es dir?", "Mir geht es gut."]
         sentences = list(generate_sentences(generator(), minimum_sentence_length = 3, context_size=5, minimum_first_fragment_length = 3, quick_yield_single_sentence_fragment=True))
